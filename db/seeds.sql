0418cfd8dbfd30648250654f78f817468814ce2e
Christian Saxton <CRS-MACBOOK@CRS-MBP.local>
2018-06-19 15:52:33 -0400

    First functional push

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('The 50 Oz burger');
+INSERT INTO burgers (burger_name) VALUES ('Pigeon and Worm Burger');
+INSERT INTO burgers (burger_name) VALUES ('Donut Burger Sliders');
+INSERT INTO burgers (burger_name) VALUES ('The Spaghetti Burger');
+INSERT INTO burgers (burger_name) VALUES ('Deep Fried Peanut Butter and Jelly Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black Bun Fried Frog Burger');
+INSERT INTO burgers (burger_name) VALUES ('Quadruple Bypass Burger');
